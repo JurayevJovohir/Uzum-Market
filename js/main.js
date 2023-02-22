3683dc2943be4e4c6a2800320bdc62847d6a25b8
JurayevJovohir <javokhake1@gmail.com>
2023-02-22 11:27:03 -0800

    canged cards

--- js/main.js
+++ js/main.js
@@ -5,14 +5,25 @@ const elLoader = findElement(".lds-spinner");
 
 let products = []
 
-const BASE_URL = 'https://fakestoreapi.com'
+const BASE_URL = 'https://fakestoreapi.com';
 
-fetch( BASE_URL + '/products')
-        .then((res) => res.json())
-        .then((res) => {
-                products = res;
-                renderProducts(products, elList);
-                elLoader.style.cssText = 'display: none!important';
-        });
+(async function getData() {
+        const res = await fetch( BASE_URL + '/products');
+        const data = await res.json();
+        products = data;
+
+        console.log(products)
+        renderProducts(products, elList);
+        elLoader.style.cssText = 'display: none!important';
+}) ()
+
+
+// fetch(BASE_URL + '/products')
+//         .then((res) => res.json())
+//         .then((res) => {
+//                 products = res;
+//                 renderProducts(products, elList);
+//                 elLoader.style.cssText = 'display: none!important';
+//         });
 
 
--- js/utils/renderProducts.js
+++ js/utils/renderProducts.js
@@ -3,6 +3,7 @@ function renderProducts(array, parent) {
     array.slice(0, 20).forEach((product) => {
             const newProduct = elTemplate.content.cloneNode(true);
 
+            console.log(product)
             const productImg = findElement('img', newProduct)
             const productTitle = findElement('#products-title', newProduct)
             const productRating = findElement('#products-rating', newProduct)
@@ -10,10 +11,10 @@ function renderProducts(array, parent) {
             const productPrice2 = findElement('#products-price-2 ', newProduct)
 
             productImg.src = product.image
-            productTitle.textContent = product.name
-            productRating.textContent = product.rating
+            productTitle.textContent = product.title.slice(1,25)
+            productRating.textContent = product.rating.rate + `(${product.rating.count} baho)`
             productPrice.textContent = product.price
-            productPrice2.textContent = product.price2
+            productPrice2.textContent = product.price
 
             elList.appendChild(newProduct)
     });
